6aa83bfb72254863b3d405ac4655d7e859b18363
beata <beata@emc2wave.com>
2019-12-19 09:16:23 -0800

    add eventListener to form

--- procedural.js
+++ procedural.js
@@ -1,3 +1,9 @@
 const form = document.getElementById('user-input');
 const userNameInput = document.getElementById('username');
 const passwortInput = document.getElementById('password');
+
+function signupHandler() {
+
+}
+
+form.addEventListener('submit', signupHandler);
