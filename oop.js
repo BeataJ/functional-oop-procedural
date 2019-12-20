761512614e44c7f143769ac2df76870b0849a389
beata <beata@emc2wave.com>
2019-12-19 22:07:04 -0800

    create UserInputForm stntax

--- oop.js
+++ oop.js
@@ -2,4 +2,18 @@ class Validator {}
 
 class User {}
 
-class UserInputForm {}
+class UserInputForm {
+  constructor() {
+    this.form = document.getElementById('user-input');
+    this.userNameInput = document.getElementById('username');
+    this.passwordInput = document.getElementById('password');
+
+    this.form.addEventListener('submit', this.signupHandler.bind(this));
+  }
+
+  signupHandler(e) {
+    e.preventDefault();
+    const enteredUserName = this.userNameInput.value;
+    const enteredPassword = this.passwordInput.value;
+    }
+}
