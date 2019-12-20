c878a4b196174322eb9d2e337ed5dee5a042204f
beata <beata@emc2wave.com>
2019-12-20 15:04:24 -0800

    add signupHandler, formID, formSubmitHadler para.

--- functional.js
+++ functional.js
@@ -1,4 +1,10 @@
-function connectForm() {
-  const form = document.getElementById('user-input');
-  form.addEventListener('submit', );
+function signupHandler(e) {
+  e.preventDefault();
 }
+
+function connectForm(formId, formSubmitHandler) {
+  const form = document.getElementById(formId);
+  form.addEventListener('submit', formSubmitHandler);
+}
+
+connectForm('user-input', signupHandler);
