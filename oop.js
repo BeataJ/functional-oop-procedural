e1c6dbc4068e5dfc8d7471b6930a4d228b84b291
beata <beata@emc2wave.com>
2019-12-19 22:49:34 -0800

    work on syntax

--- oop.js
+++ oop.js
@@ -3,23 +3,23 @@ class Validator {
   static MIN_LENGTH = 'MIN_LENGTH';
 
   static validate(value, flag, validatorValue) {
-    if(flag === this.REQUIRED) {
+    if (flag === this.REQUIRED) {
       return value.trim().length > 0;
     }
-    if(flag === this.MIN_LENGTH) {
+    if (flag === this.MIN_LENGTH) {
       return value.trim().length > validatorValue;
     }
   }
 }
 
 class User {
-  constructor(uName, uPassword){
+  constructor(uName, uPassword) {
     this.userName = uName;
     this.password = uPassword;
   }
 
   greet() {
-    console.log('hi, I am ' + this.userName);
+    console.log('Hi, I am ' + this.userName);
   }
 }
 
@@ -32,21 +32,25 @@ class UserInputForm {
     this.form.addEventListener('submit', this.signupHandler.bind(this));
   }
 
-  signupHandler(e) {
-    e.preventDefault();
+  signupHandler(event) {
+    event.preventDefault();
     const enteredUserName = this.userNameInput.value;
     const enteredPassword = this.passwordInput.value;
 
-    if(!Validator.validate(enteredUserName, Validator.REQUIRED) || !Validator.validate(enteredPassword, Validator.MIN_LENGTH, 5)) {
+    if (
+      !Validator.validate(enteredUserName, Validator.REQUIRED) ||
+      !Validator.validate(enteredPassword, Validator.MIN_LENGTH, 5)
+    ) {
       alert(
-        'Invalid input - username or password is wrong (password should be at least six chracters)'
+        'Invalid input - username or password is wrong (password should be at least six characters).'
       );
       return;
-    } 
+    }
 
     const newUser = new User(enteredUserName, enteredPassword);
     console.log(newUser);
     newUser.greet();
-
   }
 }
+
+new UserInputForm();
